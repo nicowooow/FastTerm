375623a52bdf7fed2f1965b4d8c1747738f825d0
nicowooow <nicolasgianfranco123@gmail.com>
2026-06-15 13:14:28 +0200

    para que la ventana se pueda agrandar y achicar, y de paso se intento hacer que la app agarre la CPU dedicada y no la interna, pero no se pudo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 slint::include_modules!();
 
 fn main() {
+    // hacemos que en las variables de entorno se ponga el balor de slint_backend en software, para que use una CPU
+    // unsafe {
+    //     std::env::set_var("SLINT_BACKEND", "software");
+    // }
+
     let app = MainWindow::new().unwrap();
     // .unwrap() extrae el resultado exitoso de la compilación o detiene el programa si hubo un error.
 
